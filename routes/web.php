ad207742f04fa9b42852335563579fd83e87826d
realbluea <abraaolucas19985@gmail.com>
2024-03-31 02:11:54 -0300

    Added blade.php and some controllers. Some routes were added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+
+Route::get('/login', function () {return 'login';})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () { return 'Clientes';})->name('app.clientes');
+
+    Route::get('/fornecedores', function () { return 'Fornecedores';})->name('app.fornecedores');
+
+    Route::get('/produtos', function () { return 'Produtos';})->name('app.produtos');
+});
+
+Route::get('route1', function () { return 'route1'; })->name('site.route1');
+
+Route::get('route2', function () { return redirect()->route('site.route1'); })->name('site.route2');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para voltar para a página principal.';
 });
